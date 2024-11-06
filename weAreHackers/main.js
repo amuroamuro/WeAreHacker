3914ff0874da77049fc2d9fecf5e2ca046dad0f8
amuroamuro <mbhstar1206@gmail.com>
2024-11-06 16:45:21 +0900

    Update main.js

--- weAreHackers/main.js
+++ weAreHackers/main.js
@@ -31,14 +31,14 @@ function messge() {
 }
 
 function error() {
-    alert("Holy shit! Error!")
+    alert("Holy shit! Error!");
 }
 
 function Output(text) {
     output.innerText = text;
 }
 
-function newKey(){
+function newKey() {
     key = Math.floor(Math.random() * 10 + 1);
     //Math.random 0~1 사이의 난수 생성
     //Math.floor 소수점을 내림시켜 정수로 만듦
@@ -47,94 +47,98 @@ function newKey(){
 function Run() {
     commend = typeCode.value;
     
-    if(server) {
-        if(commend=="level"){
+    // 서버가 활성화 된 경우
+    if (server) {
+        if (commend == "level") {
             Output(level.toString());
-        }else if(commend=="crash"){
-            if(level>1){
+        } else if (commend == "crash") {
+            // Level이 1보다 클 때만 crash 명령어 처리
+            if (level > 1) {
                 crashingKey = true;
                 crash = 0;
                 newKey();
                 Output("Write down password.");
-            }else{
-                error();
+            } else {
+                // Level이 1이면 crash 명령어를 사용할 수 없음
+                error();  // 에러 처리
             }
-        }else if(crashingKey){
+        } else if (crashingKey) {
             numberN = Number(commend);
             
-            if(isNaN(numberN)){
-                if(commend=="password"){
+            // 숫자 값이 아닌 경우, password 입력을 대기
+            if (isNaN(numberN)) {
+                if (commend == "password") {
                     Output(key.toString());
-                }else{
+                } else {
                     error();
                 }
-            }else{
+            } else {
                 crash = numberN;
-                if(key==numberN){
+                if (key == numberN) {
                     crashing = true;
                     crashingKey = false;
                     Output("Enter your level now");
-                }else{
+                } else {
                     Output("It's not password");
                 }
             }
-        }else if(crashing){
+        } else if (crashing) {
             numberN = Number(commend);
             
-            if(isNaN(numberN)){
-                if(commend=="level"){
+            if (isNaN(numberN)) {
+                if (commend == "level") {
                     Output(level.toString());
-                }else{
+                } else {
                     error();
                 }
-            }else{
+            } else {
                 crash = numberN;
-                if(level==numberN){
+                if (level == numberN) {
                     crashing = false;
                     crashingKey = false;
-                    Output("Complate!");
-                }else{
+                    Output("Complete!");
+                } else {
                     Output("This is not this level");
                 }
             }
-        }else if(level==1){
-            if(commend=="hack"){
+        } else if (level == 1) {
+            if (commend == "hack") {
                 hackingTool = true;
                 Output("We planted a hacking tool!!");
-            }else if(commend=="down"){
+            } else if (commend == "down") {
                 serverCash += 1;
                 Output("Server cash increases by 1!");
-            }else if(commend=="get"){
+            } else if (commend == "get") {
                 myCash += serverCash;
                 serverCash = 0;
                 server = false;
                 serverLoad = 0;
                 Output("I don't need this server anymore, so I'll throw it away.");
-                cash.innerText = "cash : " + myCash;
+                cash.innerText = "Cash : " + myCash;
             }
         }
     } else {
-        if(serverLoad==0){
-            if(commend=="server"){
+        if (serverLoad == 0) {
+            if (commend == "server") {
                 serverCash = 9;
                 serverLoad = 1;
-                Output("good! Write down your level quickly!");
-            } else{
+                Output("Good! Write down your level quickly!");
+            } else {
                 error();
             }
-        }else if(serverLoad==1){
+        } else if (serverLoad == 1) {
             numberN = Number(commend);
             
-            if(isNaN(numberN)) {
+            if (isNaN(numberN)) {
                 error();
-            }else {
-                if(numberN>0){
+            } else {
+                if (numberN > 0) {
                     serverLoad = 0;
                     server = true;
                     level = Math.floor(numberN);
                     serverCash += level;
                     Output("Server connection complete!");
-                }else{
+                } else {
                     error();
                 }
             }
@@ -144,10 +148,13 @@ function Run() {
 
 //이벤트 리스너
 helpButton.addEventListener("click", messge);
+
+//`Enter`키 처리 - 페이지 새로고침 방지 및 Run 함수 실행
 typeCode.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         event.preventDefault();  // 기본 동작인 새로고침 방지
         Run();  // Run 함수 호출
     }
 });
+
 run.addEventListener("click", Run);
